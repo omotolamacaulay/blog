f2bce77af6ad8d5f0a2ff03b96ce5a5577b2d633
Omotola Macaulay <tolamacaulay@gmail.com>
2021-02-23 17:40:14 +0100

    Build react components

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React, { useState } from 'react';
-import Axios from 'axios'
 import axios from 'axios';
 
 export default () => {
--- comments/index.js
+++ comments/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
-constcors - require('cors');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
