69db449022c8ea484c805661550dd68e0c472e6c
Rimansukwe <Rimansukwe>
2021-04-06 12:37:29 +0100

    Create moderation service

--- comments/index.js
+++ comments/index.js
@@ -6,10 +6,9 @@ const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
-const commentsByPostId = {}
-
+const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
@@ -18,20 +17,21 @@ app.get('/posts/:id/comments', (req, res) => {
 app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
-  const comments = commentsByPostId[req.params.id] || []
-  comments.push({id: commentId, content});
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content, status: 'pending' });
   commentsByPostId[req.params.id] = comments;
 
   await axios.post('http://localhost:4005/events', {
     type: 'CommentCreated',
-    data : {
+    data: {
       id: commentId,
       content,
-      postId: req.params.id
+      postId: req.params.id,
+      status: 'pending'
     }
-  })
-  
-  res.status(201).send(comments)
+  });
+
+  res.status(201).send(comments);
 });
 app.post('/events', (req, res) => {
   console.log('Received Event:', req.body.type);
